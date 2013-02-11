3ae97c443f8139d746a27fcb5ec424a0f543ee46
Reuben Thomas <rrt@sc3d.org>
2013-02-11 12:43:09 +0000

    web.pl: set environment variables in BEGIN block so they are available to other modules' intitalization code
    Fixes a warning from BaseDir.pm.

--- cgi-bin/web.pl
+++ cgi-bin/web.pl
@@ -1,11 +1,13 @@
 #! /usr/bin/perl -T
 # DarkGlass
-# (c) 2002-2009 Reuben Thomas (rrt@sc3d.org, http://rrt.sc3d.org)
+# (c) 2002-2013 Reuben Thomas (rrt@sc3d.org, http://rrt.sc3d.org)
 # Distributed under the GNU General Public License
 
 # FIXME: Remove cgi-bin from PATH
-$ENV{HOME} = '/home/rrt';
-$ENV{PATH} = '/home/rrt/public_html/cgi-bin:/usr/local/bin:/bin:/usr/bin';
+BEGIN { # Run before "use" statements so variables are defined for other modules' initialization
+  $ENV{HOME} = '/home/rrt';
+  $ENV{PATH} = "$ENV{HOME}/public_html/cgi-bin:/usr/local/bin:/bin:/usr/bin";
+}
 
 use utf8;
 use strict;
@@ -20,7 +22,7 @@ use DarkGlass;
 # Configuration
 
 # URL of server
-$DarkGlass::ServerUrl = "http://canta.dyndns.org";
+$DarkGlass::ServerUrl = "http://skwd.it.cx";
 # Root of site relative to root of server
 $DarkGlass::BaseUrl = "/~rrt/";
 # Directory of site in file system
